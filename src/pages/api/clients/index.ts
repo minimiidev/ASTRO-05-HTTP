a0352c47d92bc02ba59377fe38b2290f81fae056
unknown <javiervitalioperalta@gmail.com>
2024-12-16 20:58:21 -0400

    ...

--- src/pages/api/clients/index.ts
+++ src/pages/api/clients/index.ts
@@ -1,8 +1,6 @@
 import type { APIRoute } from "astro";
 import { db, Clients } from "astro:db";
 
-export const prerender = false;
-
 export const GET: APIRoute = async ({ params, request }) => {
   const users = await db.select().from(Clients);
 
